24af5738bd211c75851b15d8f3533c29fee19b97
pawansuthar01 <paw.kum.2111@gmail.com>
2025-05-04 17:39:14 +0530

    moudle add -> banner,destination

---
+++ backend/module/bannerModule.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const BannerModule = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    smallDescription: { type: String, required: true },
+    images: [
+      {
+        public_url: String,
+        secure_url: String,
+      },
+    ],
+    active: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Banner = mongoose.model("BannerModule", BannerModule);
+export default Banner;
---
+++ backend/module/destinationModule.js
@@ -0,0 +1,95 @@
+import mongoose from "mongoose";
+import slugify from "slugify";
+const destinationSchema = new mongoose.Schema(
+  {
+    type: { type: String, required: true },
+    name: { type: String, required: true, trim: true },
+    slug: { type: String, unique: true, lowercase: true },
+
+    thumbnail: {
+      public_id: String,
+      secure_url: String,
+    },
+
+    images: [
+      {
+        public_id: String,
+        secure_url: String,
+      },
+    ],
+
+    description: { type: String },
+    longDescription: { type: String },
+
+    category: { type: String },
+    bestTimeToVisit: { type: String },
+
+    tags: [String],
+    popularFor: [String],
+
+    location: {
+      country: String,
+      region: String,
+      coordinates: {
+        latitude: Number,
+        longitude: Number,
+      },
+    },
+
+    rating: {
+      value: { type: Number, default: 0 },
+      count: { type: Number, default: 0 },
+    },
+
+    travelTips: [String],
+
+    itinerary: [
+      {
+        day: Number,
+        title: String,
+        activities: [String],
+      },
+    ],
+
+    weatherInfo: {
+      avgTemp: String,
+      climateType: String,
+      bestMonth: String,
+    },
+
+    featured: { type: Boolean, default: false },
+    isPublished: { type: Boolean, default: false },
+
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+    reviews: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        rating: Number,
+        comment: String,
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+destinationSchema.index({
+  name: "text",
+  description: "text",
+  longDescription: "text",
+  category: "text",
+  tags: "text",
+  "location.country": "text",
+  "location.region": "text",
+  popularFor: "text",
+});
+// save  auto slug url to change destination name or new upload
+destinationSchema.pre("save", function (next) {
+  if (this.isModified("name")) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+  }
+  next();
+});
+
+export default mongoose.model("Destination", destinationSchema);
